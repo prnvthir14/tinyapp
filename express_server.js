a076b790198072f3f1eeb29d5e35129465ae8ef9
prnvthir14 <prnvthir@gmail.com>
2021-01-18 18:35:11 +0000

    updated to use ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 //initialize our app server
-const app = express ()
+const app = express ();
 
 //default port to listen to.
 const PORT = 8080;
+
+//using ejs as our apps templating engine
+app.set("view engine", "ejs");
+
 
 // test data to work with
 const urlDatabase = {
@@ -19,15 +23,21 @@ app.get('/', (req,res) => {
   res.send('Hello');
 
 });
-
+//get to <domianName>/urls.json; display json(urlDatabase)
 app.get("/urls.json", (req,res) => {
 
   res.json(urlDatabase);
 
 })
 
+//get to <domianName>/urls.json; display json(urlDatabase)
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
 
   console.log(`Example app listening on port ${PORT}!`);
 
 });
+
